a2a2f2150f457fbe1e62aaef3d64e3b75ae67767
Vi-Key <Vi-Key@ViKey>
2013-11-03 20:33:14 +0700

    commit by kien

---
+++ kien/src/kien/kien1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package kien;
+
+/**
+ * @author Vi-Key
+ *
+ */
+public class kien1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
